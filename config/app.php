e3f97415c5ea17f94126251707bd12ae04c38687
ho dev <hodev.dev@gmail.com>
2021-04-03 15:55:39 +0430

    test limit

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
   header('Access-Control-Allow-Origin', '*');
   header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  set_time_limit(100000000000000);
+  set_time_limit(90000);
 return [
 
     /*
